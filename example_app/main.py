552800e19cc3603174b69ae4a422c5ee71b7b13a
Dan Kasak <d.j.kasak.dk@gmail.com>
2025-01-13 16:27:57 +1100

    Example App:  - Restructured to be more inline with my other apps. main.py    now does all the startup stuff, and then opens Main_Window.    Other windows can be added by copying the example structure    in main_window.py

---
+++ example_app/main.py
@@ -0,0 +1,91 @@
+import gi , sqlite3 , os
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+
+from gi.repository import Gtk , Adw, GObject
+from Gtk4DbBinder import Gtk4DbDatasheet, Gtk4DbForm
+
+from main_window import Main_Window
+
+class ExampleApplication( Adw.Application ):
+
+    def __init__( self , **kwargs ):
+        super().__init__( **kwargs )
+        self.connect( 'activate' , self.on_activate )
+
+    def on_activate( self , app ):
+
+        new_setup = not( os.path.isfile( 'example.db' ) )
+        connection = sqlite3.connect( "example.db", isolation_level=None )
+        if new_setup:
+            cursor = connection.cursor()
+            cursor.execute( """
+                create table business_types (
+                    id                   integer           primary key
+                  , business_type        text not null
+                );""" )
+            customer_types = [
+                ( 'Car Sales', )
+              , ( 'Mining', )
+              , ( 'Business Services', )
+              , ( 'Religion', )
+              , ( 'Media', )
+            ]
+            cursor.executemany(
+                """insert into business_types ( business_type ) values ( ? )"""
+              , customer_types )
+            cursor.execute( """
+                create table customers (
+                    id                   integer           primary key
+                  , name                 text not null
+                  , contract_start_date  date not null
+                  , contract_end_date    date
+                  , business_type_id     integer
+                );""" )
+            customers = [
+                ( 'Dodgy Bros Car Sales', '2020-07-01', None , 1 )
+              , ( 'Hancock Prospecting', '2022-01-12', None , 2 )
+              , ( 'Trust Us Data Broker', '2003-04-01', '2013-02-14' , 3 )
+              , ( 'Followers of the Only True God inc', '2018-12-25', None , 4 )
+              , ( 'Faux Media', '1989-12-13', None , 5 )
+            ]
+            cursor.executemany(
+                """insert into customers ( name , contract_start_date , contract_end_date , business_type_id )
+                       values ( ? , ? , ? , ? )"""
+                , customers )
+            cursor.execute( """
+                create table addresses (
+                    id                  integer           primary key
+                  , customer_id         int    not null
+                  , address_line_1      text
+                  , address_line_2      text
+                  , city                text
+                  , state               text
+                  , country             text
+                  , postcode            text
+                );""" )
+            addresses = [
+                ( 1 , 'Parramatta Road' , None , 'Granville' , 'NSW' , 'Australia' , 2142 )
+              , ( 1 , 'Parramatta Road' , None , 'Auburn' , 'NSW' , 'Australia' , None )
+              , ( 2 , 'HPPL House', '28-42 Ventnor Avenue' , 'West Perth' , 'WA' , 'Australia' , 6005 )
+              , ( 3 , 'P.O.Box 549' , 'KY1-1602, Corner of Mary Street and Sheddon Road' , 'Grand Cayman' , 'CA' , 'Cayman Islands' , 'KY1-1602' )
+              , ( 4 , 'Hillsong Convention Centre' , '1 Solent Cct' , 'Norwest' , 'NSW' , 'Australia' , 2153 )
+              , ( 5 , '2 Holt Street' , None , 'Surry Hills' , 'NSW' , 'Australia' , 2010 )
+            ]
+            for i in range( 0, 200 ):
+                cursor.executemany(
+                    """insert into addresses ( customer_id, address_line_1 , address_line_2 , city, state,country, postcode )
+                           values ( ? , ? , ? , ? , ? , ? , ? )"""
+                  , addresses )
+            connection.commit()
+
+        globals = {
+            'application': self
+          , 'connection': connection
+        }
+        main_window = Main_Window( globals )
+
+app = ExampleApplication()
+
+app.run()
---
+++ example_app/main_window.py
@@ -0,0 +1,141 @@
+import gi , sqlite3 , os
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+
+from gi.repository import Gtk , Adw, GObject
+from Gtk4DbBinder import Gtk4DbDatasheet, Gtk4DbForm
+
+class Main_Window( object ):
+
+    def __init__( self , globals ):
+
+        self.globals = globals
+        self.connection = globals['connection']
+        self.builder = Gtk.Builder( self )
+        self.builder.add_from_file( 'main_window.ui' )
+        self.example_window = self.builder.get_object( 'main_window' )
+        self.example_window.set_application( globals['application'] )
+
+        self.css_provider = Gtk.CssProvider()
+
+        self.customer_form = Gtk4DbForm.generator(
+            connection = self.connection
+            , sql = {
+                "select": "*"
+              , "from": "customers"
+              , "where": "id=?"
+              , "bind_values": [ 0 ]
+            }
+          , builder = self.builder
+          , widget_prefix='customer.'
+          , css_provider=self.css_provider
+          , drop_downs = {
+               "business_type_id": {
+                   "sql": "select id , business_type from business_types"
+                 , "bind_values": []
+               }
+           }
+        )
+
+        self.addresses = Gtk4DbDatasheet.generator(
+            connection = self.connection
+          , sql = {
+                "select": "*"
+              , "from": "addresses"
+              , "where": "customer_id=?"
+              , "bind_values": [ 0 ]
+            }
+          , fields = [
+                {
+                    'name': 'id'
+                  # , 'type': 'text'
+                  # , 'x_absolute': 120
+                  , 'type': 'hidden'
+                }
+              , {
+                    'name': 'customer_id'
+                  , 'type': 'hidden'
+                }
+              , {
+                    'name': 'address_line_1'
+                  , 'type': 'text'
+                  , 'x_percent': 25
+                }
+              , {
+                    'name': 'address_line_2'
+                  , 'type': 'text'
+                  , 'x_percent': 25
+                }
+              , {
+                    'name': 'city'
+                  , 'type': 'text'
+                  , 'x_percent': 20
+                }
+              , {
+                    'name': 'state'
+                  , 'type': 'dropdown'
+                  , 'x_absolute': 120
+                }
+              , {
+                    'name': 'country'
+                  , 'type': 'text'
+                  , 'x_percent': 20
+                }
+              , {
+                    'name': 'postcode'
+                  , 'type': 'text'
+                  , 'x_percent': 10
+                }
+            ]
+          , drop_downs = {
+                'state': {
+                    'sql': 'select state , state from addresses group by 1 , 2'
+                  , 'bind_values': []
+                }
+            }
+          , box = self.builder.get_object( 'addresses_box' )
+        )
+
+        self.customer_list = Gtk4DbDatasheet.generator(
+            connection = self.connection
+              , sql = {
+                    "select": "id, name"
+                  , "from": "customers"
+                }
+              , fields = [
+                    {
+                        'name': 'id'
+                      , 'type': 'hidden'
+                    }
+                  , {
+                        'name': 'name'
+                      , 'type': 'text'
+                      , 'x_percent': 100
+                    }
+               ]
+          , box = self.builder.get_object( 'customer_list_box' )
+          , no_auto_tools_box = True
+        )
+
+        self.customer_list.bind_to_child(
+            self.customer_form
+          , [
+                {
+                    'source': 'id'
+                  , 'target': 'id'
+                }
+            ]
+        )
+
+        self.customer_list.bind_to_child(
+            self.addresses
+          , [
+                {
+                    'source': 'id'
+                  , 'target': 'customer_id'
+                }
+            ]
+        )
+
+        self.example_window.present()
